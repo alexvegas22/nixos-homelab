ba0e5fa1bb9b0e0c5b9ff3dea601986fd23ea8a2
Alex Vegas <v34l@proton.me>
2025-12-30 23:14:21 -0500

    extra config

--- dns/services.nix
+++ dns/services.nix
@@ -8,28 +8,50 @@
 
     nginx = {
       enable = true;
+
       virtualHosts = {
         "v34l.com" = {
           enableACME = true;
           forceSSL = true;
+
           locations."/" = {
             proxyPass = "https://192.168.3.1";
             proxyWebsockets = true;
-            extraConfig = "proxy_ssl_server_name on;" + "proxy_pass_header Authorization;";
+
+            extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Forwarded-Proto https;
+          proxy_set_header X-Forwarded-For $remote_addr;
+
+          proxy_ssl_server_name on;
+          proxy_ssl_name v34l.com;
+          proxy_ssl_verify off;
+        '';
           };
         };
 
         "jg1g.com" = {
           enableACME = true;
           forceSSL = true;
+
           locations."/" = {
             proxyPass = "https://192.168.1.5";
             proxyWebsockets = true;
-            extraConfig = "proxy_ssl_server_name on;" + "proxy_pass_header Authorization;";
+
+            extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Forwarded-Proto https;
+          proxy_set_header X-Forwarded-For $remote_addr;
+
+          proxy_ssl_server_name on;
+          proxy_ssl_name jg1g.com;
+          proxy_ssl_verify off;
+        '';
           };
         };
       };
     };
+
     openssh.enable = true;
     dnsmasq = {
       enable = true;
