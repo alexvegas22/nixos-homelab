2fc6de20830fab4f342abdaf0ac1c9049f433987
v34l <v34l@proton.me>
2026-02-09 00:34:03 -0500

    reorder dns entries

--- dns/services.nix
+++ dns/services.nix
@@ -37,7 +37,11 @@
       alwaysKeepRunning = true;
       settings = {
         server = [ "1.1.1.1" ];
-        address = [ "/.v34l.com/192.168.3.1" "/.jg1g.com/192.168.1.5" "home.v34l.com/192.168.2.52" ];
+        address = [
+          "/home.v34l.com/192.168.2.52"
+          "/.v34l.com/192.168.3.1"
+          "/.jg1g.com/192.168.1.5"
+        ];
       };
     };
   };
